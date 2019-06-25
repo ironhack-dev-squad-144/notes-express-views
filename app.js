7338d7761aa5708bceabb9062327fcf452b6258e
Maxence Bouret <maxence.bouret@gmail.com>
2019-06-25 16:55:22 +0100

    Update

--- app.js
+++ app.js
@@ -3,11 +3,27 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
+
+// Define the views folder
 app.set("views", __dirname + "/views");
+
+// Define the view engine: HBS
+// HBS is like HTML++. Express can use some variables and conditions and loops in a HBS file
 app.set("view engine", "hbs");
 
+// Route "/" (http://localhost:3000)
 app.get("/", (req, res, next) => {
-  res.render("index");
+  let h = new Date().getHours();
+  let cities = ["Lisbon", "Porto", "Madrid", "Barcelona", "Paris"];
+  cities.sort();
+
+  // Render the view "/views/index.hbs"
+  res.render("index", {
+    hours: h,
+    minutes: new Date().getMinutes(),
+    isDaylight: 6 <= h && h <= 20,
+    cities: cities,
+  });
 });
 
 app.listen(3000, () => {
